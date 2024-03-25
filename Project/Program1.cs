59063df8080eb68348c39b199073b73f1aed2a41
Никита Жирнов <nikitazirnov15@gmail.com>
2024-03-25 20:40:59 +0300

    что то

---
+++ Project/Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    static class Programm1
+    {
+        public static void main()
+        {
+            int[] arr = new int[10]
+                .Select(x => new Random().Next(100)).ToArray().Print();
+            int[] arr1 = arr.OrderBy(x => x).ToArray().Print();
+            int[] arr2 = arr.OrderByDescending(x => x).ToArray().Print();
+            if (arr.SequenceEqual(arr1))
+                "OrderBy".Print();
+            else if (arr.SequenceEqual(arr2))
+                "OrderByDescending".Print();
+            else
+                "random".Print();
+
+        }
+    }
+}
